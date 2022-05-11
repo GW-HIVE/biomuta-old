5381b9cf81428b7effac1b7925f5beb2421c3128
Ned Cauley <escauley@users.noreply.github.com>
2022-05-11 18:46:08 -0400

    Update convert_civic_vcf.py

--- downloader/civic/convert_civic_vcf.py
+++ downloader/civic/convert_civic_vcf.py
@@ -15,7 +15,7 @@ Usage:
 
     *Gives a description of the neccessary commands
 
-    * python convert_civic_vcf -i <path/input_file.vcf> -s <path/schema.json> -p output_prefix_name -o <path/>
+    * python convert_civic_vcf.py -i <path/input_file.vcf> -s <path/schema.json> -p output_prefix_name -o <path/>
 
     *Runs the script with the given input vcf and outputs a json file.
 
@@ -58,18 +58,14 @@ def main(input_vcf_file, schema, output_prefix, output_folder):
                 
                 # Record number of subfields in the INFO column
                 if re.search(r'##INFO=<ID=CSQ', str(row)):
-                    sub_field_total = str(row).count('|')
-                    #print("The INFO column contains " + str(sub_field_total) + " fields")
-
-
-            
+                    sub_field_total = str(row).count('|') + 1
+                    print(sub_field_total)
+                    #print("The INFO column contains " + str(sub_field_total) + " fields")   
             
             # Pull out header for mutation data
             elif re.search(r'#CHROM', str(row)):
                 
                 mut_fields = row
-
-            #elif re.search(r'fileformat', str(row)):
                 
             #    print(row)
             
@@ -78,31 +74,29 @@ def main(input_vcf_file, schema, output_prefix, output_folder):
         
     # Start a counter and end count used for troubleshooting
     count = 0 
-    stop_count = 3
+    stop_count = 0
     
     # A list of mutation dictionaries that will be converted into the output json
     dict_list = []
+
+    # Load the annotation subfield schema
+    with open(schema) as schema_file: 
+        annotation_schema_template = json.load(schema_file)
     
     # Convert each line in the mutation data to json format
     for line in data: 
 
         # Count the number of sub_fields in the info column
-        sub_field_count = str(line).count('|')
+        # Formula compensates for the number of '|' being always 1 fewer for each annotation added
+        sub_field_count = str(line).count('|') + int((str(line).count('|')/(sub_field_total - 1 ))) 
+        print(sub_field_count)
 
-        #print("Line " + str(count) + " contains " + str(int(sub_field_count/sub_field_total)) + " annotations")
-        #print(str(sub_field_count/sub_field_total))
-
-        # A dictionary to hold mutation data
+        # A dictionary to hold all mutation data
         mut_dict = {}
 
-        # A list and dictionary to hold the info subfields
+        # A dictionary to hold the annotation subfields
         annotation_count = int(sub_field_count/sub_field_total)
         annotation_info_dict = {}
-
-        # Add annotations to the 
-        #annotation_number = 0
-        #if annotation_number < annotation_count:
-        #    annotation_number =+ 1
 
         # Link each column in the mutation line with a field
         for field, mutation_info in zip(mut_fields, line):
@@ -117,19 +111,33 @@ def main(input_vcf_file, schema, output_prefix, output_folder):
 
                 # Annotation information is delimited by a "|"
                 annotation_info = mutation_info.split("|")
-                
-                sub_field_counter = 0
 
                 # Store the subfields as a list in each annotation object in the annotation dict
                 for annotation_index in range(annotation_count):
 
-                    #print("annotation_index " + str(annotation_index) + " out of " + str(annotation_count))
+                    annotation = annotation_info[annotation_index*(sub_field_total):(annotation_index+1)*(sub_field_total)]
 
-                    annotation_info_dict[annotation_index] = annotation_info[annotation_index*(sub_field_total):(annotation_index+1)*(sub_field_total-1)]
-                
-                #print(annotation_info_dict)
-                #print("---------------------")
+                    print(mutation_info)
+                    print(annotation_info)
+                    print(annotation)
 
+                    sub_field = 0
+
+                    sub_field_dict = {}
+
+                    for key in annotation_schema_template:
+                        print(key)
+                        print(sub_field)
+                        if sub_field == (sub_field_total): 
+                            break
+                        else:
+                            print(sub_field)
+                            print(annotation[sub_field])
+                            sub_field_dict[key]= annotation[sub_field]
+                            sub_field += 1
+
+                    annotation_info_dict[annotation_index] = sub_field_dict
+                    #annotation_info_dict[annotation_index] = annotation
                 
                 # Add the annotation dictionary to the annotation list
                 mut_annotation_list.append(annotation_info_dict)
@@ -142,7 +150,6 @@ def main(input_vcf_file, schema, output_prefix, output_folder):
             mut_dict["INFO"] = mut_annotation_list
         
         dict_list.append(mut_dict)
-        #print(dict_list)
 
         # Only go through a limited number of rows for troubleshooting
         count += 1
@@ -154,11 +161,7 @@ def main(input_vcf_file, schema, output_prefix, output_folder):
 
     with open(output_json, 'w', encoding = 'utf-8') as output_json:
         json.dump(dict_list, output_json, indent=4)
-    output_json.close()
-
-
-    #print(dict_list)
-    
+    output_json.close()    
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Commands for civic vcf to csv convertor.')
