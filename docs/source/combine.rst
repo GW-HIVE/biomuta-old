fe2f58a8abc8d7979ce4bb5a253c5ba7124df34e
Ned Cauley <escauley@users.noreply.github.com>
2022-08-25 15:14:53 -0400

    Added combine step doc

--- docs/source/combine.rst
+++ docs/source/combine.rst
@@ -1,5 +1,69 @@
-Step 3: Combine
-===============
+**Step 3: Combine**
+===================
 
 All resources are combined together into a master dataset. 
+
+**Scripts**
+-----------
+
+combine_csv.py
+
+**Procedure**
+-------------
+
+*Run combine_csv.py*
+^^^^^^^^^^^^^^^^^^^^
+
+*Summary*
+"""""""""
+
+All of the mutation data for each source was converted to a standardized data structure in the convert step.
+
+Now, all of these separate csv files (one for each source) will be combined into a master csv
+
+All csv files to be combined should be in a folder together with no additional csv files
+
+*Script Specifications*
+"""""""""""""""""""""""
+
+The script must be called from the command line and takes specific command line arguments
+
+Input
+#####
+    * -i : The folder containing csv mutation files to combine
+    * -o : The folder to output the combined mutation file
+
+Output
+######
+    * A csv file combining all csv files in a given folder
+
+Usage
+#####
+    * python combine_csv.py -h
+
+    *Gives a description of the neccessary commands
+
+    * python combine_csv.py -i <path/> -o <path/>
+
+    *Runs the script with the given folder and combines all csv files in that folder
+
+*Additional Notes*
+------------------
+
+**Final fields**
+^^^^^^^^^^^^^^^^
+
+Field,Description
+sample_name,Sample ID provided by the original resource (for v-5.0 only applies to TCGA and COSMIC)
+chr_id,Chromosome number only (no 'chr')
+start_pos,Genomic coordinates (For v-5.0 these are all with ref GRCh38)
+end_pos,Identical to the start positoon because all mutations are Specifications
+ref_nt,Reference nucleotide
+alt_nt,Nucleotide mutation
+aa_pos,Amino acide number of the amino acide change in the human_protein_transcriptlocus
+ref_aa,Reference amino acid
+alt_aa,Amino acid variation caused by the mutation
+do_name,DO parent term
+uniprot_canonical_ac,Uniprot accession for the specific ENST or ENSP listed from the source
+source,Original data source of the mutation
 
--- pipeline/combine_step3/combine_csv.py
+++ pipeline/combine_step3/combine_csv.py
@@ -2,6 +2,7 @@
 Input:
 ########
     * -i : The folder containing csv mutation files to combine
+    * -o : The folder to output the combined mutation file
 
 Output:
 ########
@@ -13,7 +14,7 @@ Usage:
 
     *Gives a description of the neccessary commands
 
-    * python combine_csv.py -i <path/>
+    * python combine_csv.py -i <path/> -o <path/>
 
     *Runs the script with the given folder and combines all csv files in that folder
 
@@ -26,7 +27,7 @@ import pandas as pd
 
 
 
-def main(input_folder):
+def main(input_folder, output_folder):
     
     # Grab and combine all of the csv files in the specified folder
     final_mutation_files = os.path.join(input_folder, "*.csv")
@@ -44,9 +45,10 @@ def main(input_folder):
     with_dup_total = len(final_df)
     final_df.drop_duplicates(keep='first', inplace=True)
     dup_number = len(final_df.index) - with_dup_total
-    print("Removed " + str(dup_number) + " duplicate rows") 
+    if dup_number > 0:
+        print("Removed " + str(dup_number) + " duplicate rows") 
 
-    final_file_path = input_folder + "/biomuta_v5.csv"
+    final_file_path = output_folder + "/biomuta_v5.csv"
     print("Exporting mapped file to " + final_file_path)
     final_df.to_csv(final_file_path, index = False)
 
@@ -54,9 +56,11 @@ def main(input_folder):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Commands for csv combine script')
     parser.add_argument('--input_folder', '-i',
-                        help='An absolute path to folder containing csv files to combine')
+                        help='An absolute path to a folder containing csv files to combine')
+    parser.add_argument('--output_folder', '-o',
+                        help='An absolute path to a folder where the final combined mutation file will be written')
     args = parser.parse_args()
 
-    main(args.input_folder)
+    main(args.input_folder, args.output_folder)
 
-#python combine_csv.py -i /mnt/c/Users/caule/OncoMX/biomuta/v-5.0/compiled/
+#python combine_csv.py -i /mnt/c/Users/caule/OncoMX/biomuta/v-5.0/compiled/source_mutation_files -o /mnt/c/Users/caule/OncoMX/biomuta/v-5.0/compiled
