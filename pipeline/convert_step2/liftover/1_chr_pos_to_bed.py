72cb8a5c4fafe7450d180793a22c4aa0042caf07
mariacuria <marianatkim-sch@outlook.com>
2024-11-21 09:07:01 -0500

    Added fields extracted with 1_chr_pos_to_bed.py

--- pipeline/convert_step2/liftover/1_chr_pos_to_bed.py
+++ pipeline/convert_step2/liftover/1_chr_pos_to_bed.py
@@ -25,19 +25,54 @@ def process_json_to_bed(input_directory, output_bed_file):
                     # Process each JSON object in the file
                     for record in data:
                         try:
+                            # Check genome build and position length criteria
+                            if record.get('ncbiBuild') not in ['GRCh37', '37']:
+                                continue
+                            if record.get('variantType') != 'SNP': #take SNPs only
+                                continue
+                            if record['endPosition'] - record['startPosition'] != 0: #additional check to confirm SNP
+                                continue
+                            if 'splice' in record.get('proteinChange', ''):  # check if 'proteinChange' contains 'splice'
+                                continue
+
+#To see all available values for a given json key such as 'ncbiBuild': GRCh37, 37, 'variantType': SNP..., run extract_records.py
+#variantType counts:
+#SNP: 34247933
+#DNP: 159984
+#DEL: 2002207
+#INS: 797275
+#TNP: 2541
+#ONP: 4666
+#NA: 3540
+#MNP: 1812
+#UNK: 3
+#Del: 89
+
+#ncbiBuild counts:
+#37: 37829
+#GRCh37: 30925146
+#NA: 3143
+#GRCh38: 6253932
+                            
                             # Extract chromosome, start position, and end position
+
                             chr_ = record['chr']
-                            # Replace chr23 with chrX
+                            # Convert specific chromosome values and exclude unwanted chromosomes
                             if chr_ == '23':
                                 chr_ = 'X'
-                            # Ensure chromosome name starts with 'chr' (if not already)
+                            if chr_ == '24':
+                                chr_ = 'Y'
+                            if chr_ in ['MT', 'NA']:
+                                continue # Skip records with 'MT' (mitochondrial) or 'NA' as chromosome values
                             if not chr_.startswith('chr'):
                                 chr_ = 'chr' + chr_
                             start_pos = record['startPosition'] - 1  # Convert to 0-based for BED
                             end_pos = record['endPosition']
+                            protein_change = record['proteinChange']
+                            unique_sample_key = record['uniqueSampleKey']
 
                             # Append line to buffer
-                            buffer.append(f"{chr_}\t{start_pos}\t{end_pos}\n")
+                            buffer.append(f"{chr_}\t{start_pos}\t{end_pos}\t{protein_change}\t{unique_sample_key}\n")
 
                             # Write buffer to file when it reaches the specified size
                             if len(buffer) >= buffer_size:
@@ -73,5 +108,5 @@ config_obj = get_config()
 dl_dir = Path(config_obj["relevant_paths"]["downloads"])
 out_dir = Path(config_obj["relevant_paths"]["generated_datasets"])
 input_directory = dl_dir / 'cbioportal' / '2024_10_21' / 'mutations'  # Write a util to get latest dir
-output_bed_file = out_dir / '2024_10_22' / 'hg19positions.bed' #Write a util to get latest dir
+output_bed_file = out_dir / '2024_10_22' / 'liftover' / 'hg19withID.bed' #Write a util to get latest dir
 process_json_to_bed(input_directory, output_bed_file)
--- pipeline/convert_step2/liftover/2_liftover.sh
+++ pipeline/convert_step2/liftover/2_liftover.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-./liftOver /data/shared/biomuta/generated/datasets/2024_10_22/liftover/unmapped_ensembl.bed ucscHg19ToHg38.over.chain /data/shared/biomuta/generated/datasets/2024_10_22/liftover/hg38positions_unmapped_by_ensembl.bed /data/shared/biomuta/generated/datasets/2024_10_22/liftover/unmapped_ucsc2.bed
+#awk '{print $1, $4, $5}' hg19withID.bed | sort | uniq -c | awk '$1 > 1'
+
+./liftOver /data/shared/biomuta/generated/datasets/2024_10_22/liftover/hg19withID.bed ucscHg19ToHg38.over.chain /data/shared/biomuta/generated/datasets/2024_10_22/liftover/ucsc_hg38withID.bed /data/shared/biomuta/generated/datasets/2024_10_22/liftover/ucsc_unmapped_withID.bed
+
+./liftOver /data/shared/biomuta/generated/datasets/2024_10_22/liftover/ucsc_unmapped_withID.bed ensembl_GRCh37_to_GRCh38.chain /data/shared/biomuta/generated/datasets/2024_10_22/liftover/ensembl_hg38withID.bed /data/shared/biomuta/generated/datasets/2024_10_22/liftover/ensembl_unmapped_withID.bed
