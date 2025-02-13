4ac8e60847ae6a9a6170db7d8e5611b6a0b0643a
mariacuria <marianatkim-sch@outlook.com>
2025-02-13 11:27:58 -0500

    Added versioning logic

--- pipeline/combine_step3/combine_csv.py
+++ pipeline/combine_step3/combine_csv.py
@@ -24,7 +24,30 @@ import argparse
 import os
 import glob
 import pandas as pd
+import re
 
+
+
+def get_next_version_number(output_folder, base_filename):
+    """
+    Finds the highest version number in the output folder for files matching the base filename and returns the next version number.
+    """
+    # Regex pattern to match "biomuta_vX.csv"
+    pattern = re.compile(rf"{re.escape(base_filename)}_v(\d+)\.csv$")
+
+    max_version = 0
+
+    # Iterate through files in the output folder
+    for filename in os.listdir(output_folder):
+        match = pattern.match(filename)
+        if match:
+            # Extract the version number from the filename
+            version = int(match.group(1))
+            if version > max_version:
+                max_version = version
+
+    # Return the next version number
+    return max_version + 1
 
 
 def main(input_folder, output_folder):
@@ -48,9 +71,12 @@ def main(input_folder, output_folder):
     if dup_number > 0:
         print("Removed " + str(dup_number) + " duplicate rows") 
 
-    final_file_path = output_folder + "/biomuta_v6.csv"
+    base_filename = "biomuta"
+    next_version = get_next_version_number(output_folder, base_filename)
+    new_filename = f"{base_filename}_v{next_version}.csv"
+    final_file_path = os.path.join(output_folder, new_filename)
     print("Exporting mapped file to " + final_file_path)
-    final_df.to_csv(final_file_path, index = False)
+    final_df.to_csv(final_file_path, index = False, quoting=1)
 
                         
 if __name__ == "__main__":
